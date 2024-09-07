bbd702d86ca10c20416475a5b0baaa386880b6a2
jiangKerman <jiangKerman@outlook.com>
2024-09-07 17:10:44 +0800

    添加了requirements

--- main.py
+++ main.py
@@ -23,21 +23,25 @@ def convert_to_webp(image_path: str):
     # 获取无损压缩的大小
     lossless_size = temp_lossless.tell()
     # print(f'{lossy_size}    {lossless_size}')
+    模式=""
     if lossy_size < lossless_size:
         # 有损压缩文件更小，使用有损压缩保存
         # 由于已经保存过一次有损压缩，只需将流中的数据写入文件
         with open(f'{image_path.rsplit(".", 1)[0]}.webp', 'wb') as file:
             temp_lossy.seek(0)  # 确保从文件开头开始写入
             file.write(temp_lossy.read())
+        模式='有损压缩'
         # print("选择了有损压缩，文件大小：{} 字节".format(lossy_size))
     else:
         with open(f'{image_path.rsplit(".", 1)[0]}.webp', 'wb') as file:
             temp_lossless.seek(0)  # 确保从文件开头开始写入
             file.write(temp_lossless.read())
         # print("无损压缩，文件大小：{} 字节".format(lossless_size))
+        模式='无损压缩'
     # 关闭临时流
     temp_lossy.close()
     temp_lossless.close()
+    return 模式
 
 def convert_to_webp_from_folder(folder_path:str):
     img_paths = []
@@ -49,7 +53,7 @@ def convert_to_webp_from_folder(folder_path:str):
     # print(img_paths)
 
     for imgpath in img_paths:
-        convert_to_webp(imgpath)
-        print(imgpath)
+        压缩模式 = convert_to_webp(imgpath)
+        print(imgpath,压缩模式)
 
-convert_to_webp_from_folder('imgs')
+convert_to_webp_from_folder(r'C:\jiangkermans\blogHexoSource\source\_posts\用廉价的方案来加速博客访问——简单的nginx转发')
